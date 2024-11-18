1232ef22b4aea9c02d1958828be2ea5f47e80379
ömer faruk dilek <omer.farukdilek@hotmail.com>
2024-11-18 21:16:32 +1100

    i updated about page

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,10 +8,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Omer Faruk Dilek </span>
+            from <span className="purple"> Melbourne, Australia.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently a student at Laneway Edu.
             <br />
             I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
             Mesra.
@@ -34,7 +34,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Omer Faruk Dilek</footer>
         </blockquote>
       </Card.Body>
     </Card>
